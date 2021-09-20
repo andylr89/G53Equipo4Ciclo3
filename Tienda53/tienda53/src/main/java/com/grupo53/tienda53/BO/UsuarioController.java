9127c127f70b36629b32a22ff86aa1c5d50f8c7e
ingenierocarlos <90942688+ingenierocarlos@users.noreply.github.com>
2021-09-20 17:25:15 -0500

    Modificación actualizarusuarios
    Se modifica la sentencia Dao.registrarUsuario(user);  por la sentencia Dao.actualizarUsuario(user); ya que esta corresponde al metodo que se quiere implementar.

--- Tienda53/tienda53/src/main/java/com/grupo53/tienda53/BO/UsuarioController.java
+++ Tienda53/tienda53/src/main/java/com/grupo53/tienda53/BO/UsuarioController.java
@@ -50,7 +50,7 @@ public class UsuarioController {
 	@PutMapping("/actualizarusuarios")
 	public void actualizarUsuario(UsuarioVO user) {
 		UsuarioDAO Dao = new UsuarioDAO();
-		Dao.registrarUsuario(user);
+		Dao.actualizarUsuario(user);
 	}
 	
 	
