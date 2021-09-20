3990d89d019732ab99d182ae5da5fc158169bd4b
ingenierocarlos <90942688+ingenierocarlos@users.noreply.github.com>
2021-09-20 14:13:10 -0500

    Proyecto con enlace a BD
    Se sube el proyecto con la información de la tabla usuarios enlazada en DTO,DAO Y BO.

---
+++ Tienda53/tienda53/src/test/java/com/grupo53/tienda53/Tienda53ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo53.tienda53;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tienda53ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
