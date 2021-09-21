3a93faccf9310a37810b07ed4f31d3b82120dfab
JOSIRAJIM <30900836+JOSIRAJIM@users.noreply.github.com>
2021-09-20 22:43:40 -0500

    Add files via upload

---
+++ tienda53_ult/src/main/java/com/grupo53/tienda53/BO/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.grupo53.tienda53.BO;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Tienda53Application.class);
+	}
+
+}
---
+++ tienda53_ult/src/main/java/com/grupo53/tienda53/DAO/Conexion.java
@@ -0,0 +1,55 @@
+package com.grupo53.tienda53.DAO;
+
+import java.sql.*;
+
+/**
+ * Clase que permite conectar con la base de datos
+ * 
+ *
+ */
+public class Conexion {
+	
+	/** Parametros de conexion */
+	static String nombre_base_datos = "minitic";
+	static String usuariobd = "root";
+	static String clavebd = "mintic";
+	static String url = "jdbc:mysql://localhost/" + nombre_base_datos;
+	
+	//objeto sin inicializar de la conexión
+	Connection connection = null;
+
+	/** Constructor de DbConnection */
+	public Conexion() {
+		try {
+			// obtenemos el driver de para mysql
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// obtenemos la conexión
+			connection = DriverManager.getConnection(url, usuariobd, clavebd);
+			
+			//si hay conexión correcta mostrar información en consola
+			if (connection != null) {
+				System.out.println("Conexión a base de datos " + nombre_base_datos + " OK\n");
+			}
+		
+		} catch (SQLException e) {
+			//error de la base de datos
+			System.out.println(e);
+		} catch (ClassNotFoundException e) {
+			//error en carga de clases
+			System.out.println(e);
+		} catch (Exception e) {
+			//cualquier otro error
+			System.out.println(e);
+		}
+	}
+
+	/** Permite retornar la conexión */
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	//cerrando la conexión
+	public void desconectar() {
+		connection = null;
+	}
+}
