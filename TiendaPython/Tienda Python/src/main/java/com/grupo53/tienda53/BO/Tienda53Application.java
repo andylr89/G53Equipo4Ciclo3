a3b725e429f821ac97ee3f534ac63055730bb9b4
ingenierocarlos <90942688+ingenierocarlos@users.noreply.github.com>
2021-09-22 22:18:43 -0500

    Proyecto completo
    Se sube el proyecto completo nuevamente con los ajustes realizados por el compañero Andrés Lambrano.

---
+++ TiendaPython/Tienda Python/src/main/java/com/grupo53/tienda53/BO/Tienda53Application.java
@@ -0,0 +1,13 @@
+package com.grupo53.tienda53.BO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda53Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda53Application.class, args);
+	}
+
+}
