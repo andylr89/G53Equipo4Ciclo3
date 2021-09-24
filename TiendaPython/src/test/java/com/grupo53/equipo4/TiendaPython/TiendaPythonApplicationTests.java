0dd0f3e68678d456667735616bae92322877318e
Diego Rodriguez <diegof.rodriguezc@ecci.edu.co>
2021-09-23 20:32:08 -0500

    Cambio total de proyecto

---
+++ TiendaPython/src/test/java/com/grupo53/equipo4/TiendaPython/TiendaPythonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo53.equipo4.TiendaPython;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaPythonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
