eca0b504b292daee8985d611a26033b4a55fe0fd
andylr89 <andreslr1989@gmail.com>
2021-09-30 20:48:54 -0500

    ProveedorVO y ProductoVO
    Agregados

---
+++ TiendaPython/src/main/java/com/grupo53/tienda53/DTO/ProveedorVO.java
@@ -0,0 +1,82 @@
+package com.grupo53.tienda53.DTO;
+
+import java.io.Serializable;
+
+public class ProveedorVO implements Serializable {
+	
+	private static final long SerialVersion=1L;
+	
+	private Integer nit_proveedor;
+	private String ciudad_proveedor;
+	private String direccion_proveedor;
+	private String nombre_proveedor;
+	private String telefono_proveedor;
+	/**
+	 * @return the nit_proveedor
+	 */
+	public Integer getNit_proveedor() {
+		return nit_proveedor;
+	}
+	/**
+	 * @param nit_proveedor the nit_proveedor to set
+	 */
+	public void setNit_proveedor(Integer nit_proveedor) {
+		this.nit_proveedor = nit_proveedor;
+	}
+	/**
+	 * @return the ciudad_proveedor
+	 */
+	public String getCiudad_proveedor() {
+		return ciudad_proveedor;
+	}
+	/**
+	 * @param ciudad_proveedor the ciudad_proveedor to set
+	 */
+	public void setCiudad_proveedor(String ciudad_proveedor) {
+		this.ciudad_proveedor = ciudad_proveedor;
+	}
+	/**
+	 * @return the direccion_proveedor
+	 */
+	public String getDireccion_proveedor() {
+		return direccion_proveedor;
+	}
+	/**
+	 * @param direccion_proveedor the direccion_proveedor to set
+	 */
+	public void setDireccion_proveedor(String direccion_proveedor) {
+		this.direccion_proveedor = direccion_proveedor;
+	}
+	/**
+	 * @return the nombre_proveedor
+	 */
+	public String getNombre_proveedor() {
+		return nombre_proveedor;
+	}
+	/**
+	 * @param nombre_proveedor the nombre_proveedor to set
+	 */
+	public void setNombre_proveedor(String nombre_proveedor) {
+		this.nombre_proveedor = nombre_proveedor;
+	}
+	/**
+	 * @return the telefono_proveedor
+	 */
+	public String getTelefono_proveedor() {
+		return telefono_proveedor;
+	}
+	/**
+	 * @param telefono_proveedor the telefono_proveedor to set
+	 */
+	public void setTelefono_proveedor(String telefono_proveedor) {
+		this.telefono_proveedor = telefono_proveedor;
+	}
+	
+	
+	
+	
+	}
+	
+	
+
+
