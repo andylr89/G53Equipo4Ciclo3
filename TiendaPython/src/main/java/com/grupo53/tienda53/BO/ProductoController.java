e5d5133746bc73fc4a25907cf9828ff99ee8049a
JOSIRAJIM <30900836+JOSIRAJIM@users.noreply.github.com>
2021-10-05 13:15:05 -0500

    Revert "Revert "Revert "Revert "Merge branch 'Andres' of https://github.com/andylr89/G53Equipo4Ciclo3 into Andres""""
    This reverts commit 08c2c053a5238495c54a043d80f851983329fa14.

---
+++ TiendaPython/src/main/java/com/grupo53/tienda53/BO/ProductoController.java
@@ -0,0 +1,62 @@
+package com.grupo53.tienda53.BO;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo53.tienda53.DAO.ProductoDAO;
+import com.grupo53.tienda53.DTO.ProductoVO;
+
+
+
+@RestController
+public class ProductoController {
+	
+	/*
+	 * @GetMapping obtener o buscar
+	 * @PostMapping insertar o agregar
+	 * @PutMapping modificar o actualizar
+	 * @DeleteMapping eliminar o borrar
+	 * */
+
+	@PostMapping("/registrarproducto")
+	public void registrarProducto(ProductoVO product) {
+		ProductoDAO Dao = new ProductoDAO();
+		Dao.registrarProducto(product);
+	}
+
+	@GetMapping("/consultarproducto")
+	public ArrayList<ProductoVO> consultarProducto(Integer code) {
+		ProductoDAO Dao = new ProductoDAO();
+		return Dao.consultarProducto(code);
+	}
+
+	@GetMapping("/listarproductos")
+	public ArrayList<ProductoVO> listaDeProductoes() {
+		ProductoDAO Dao = new ProductoDAO();
+		return Dao.listaDeProductos();
+	}
+	
+	@DeleteMapping("/eliminarproducto")
+	public void eliminarProducto(Integer code) {
+		ProductoDAO Dao = new ProductoDAO();
+		Dao.eliminarProducto(code);
+	}
+	
+	@DeleteMapping("/eliminartodoproducto")
+	public void eliminarTodoProducto() {
+		ProductoDAO Dao = new ProductoDAO();
+		Dao.eliminarTodoProducto();
+	}
+	
+	@PutMapping("/actualizarproducto")
+	public void actualizarProducto(ProductoVO product) {
+		ProductoDAO Dao = new ProductoDAO();
+		Dao.actualizarProducto(product);
+	}
+
+}
